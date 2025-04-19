0fe23820856a0b3e8bc92fbaea76663f509ea353
HyebeenLee(Emma) <bin020129@gmail.com>
2025-04-19 17:51:37 +0900

    Initial Commit

---
+++ C2_Failure_C2/C2_Failure_C2App.swift
@@ -0,0 +1,17 @@
+//
+//  C2_Failure_C2App.swift
+//  C2_Failure_C2
+//
+//  Created by 이혜빈 on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct C2_Failure_C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
